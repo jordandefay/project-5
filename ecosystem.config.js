c3dd5e1c2a68eddbe9d150e85164fa4eb3c5ace5
Jordan Defay <j.defay@chamalhat.com>
2025-06-29 18:26:26 +0200

    Corriger les permissions et démarrer PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,9 +12,10 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
-    log_file: './logs/combined.log',
+    // Supprimons temporairement les logs pour éviter les problèmes de permissions
+    // error_file: './logs/err.log',
+    // out_file: './logs/out.log',
+    // log_file: './logs/combined.log',
     time: true
   }]
 }
